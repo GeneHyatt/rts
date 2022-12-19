8e0d594a2aada26ff7af383b359308dc416ec5a2
Gene Hyatt <gene.hyatt@gmail.com>
2022-12-19 13:20:55 -0500

    Adding the lessons on state, classes and refs

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import ReactDOM from "react-dom/client";
 import GuestList from './state/GuestList';
+import UserSearch from './refs/UserSearch';
+import EventComponent from "./events/EventComponent";
+
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el!);
@@ -9,6 +12,10 @@ const App = () => {
     return (
         <div>
             <GuestList />
+            <UserSearch />
+            <br/>
+            <br/>
+            <EventComponent />
         </div>
     );
 };
